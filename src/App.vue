6c7ad51fa02e9b212146e281462b72334a84ab23
Jean Paul <jduarte@sistemasmitdev.com>
2024-05-14 15:35:05 -0500

    main carousel added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <Header></Header>
-
+  <MainCarousel></MainCarousel>
 </template>
 
 
 <script setup>
 import Header from '@/components/Header.vue'
+import MainCarousel from '@/components/MainCarousel.vue'
 </script>
---
+++ src/components/MainCarousel.vue
@@ -0,0 +1,20 @@
+<template>
+    <Carousel :opts="carouselOptions" class="relative w-full ">
+        <CarouselContent>
+            <CarouselItem v-for="(_, index) in 5" :key="index">
+                <img class="w-full"
+                    src="https://media.maxaroma.com/9cd29e92-4b8f-436f-ad1f-8e368b30dc1d/https://www.maxaroma.com/images/homeimg/banner_72024-05-13-17-05-06.jpg?ver=1715619966"
+                    alt="">
+            </CarouselItem>
+        </CarouselContent>
+    </Carousel>
+</template>
+
+<script setup lang="ts">
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel'
+
+const carouselOptions = {
+    // align: 'start',
+    loop: true,
+}
+</script>
